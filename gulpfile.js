4a7cde11b6134fcc675e037d7741d30259a1f495
thomas.toledo <thomas.toledo@eemi.com>
2020-02-29 03:01:18 +0100

    tailwind, gulp and upgrade alpinejs

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,39 @@ const { src, dest, watch, task } = require('gulp');
 const bro = require('gulp-bro');
 const browserSync = require('browser-sync').create();
 const del = require('del');
+const postcss = require('gulp-postcss');
+
+function _tailwindcss() {
+    return src('src/css/*')
+    .pipe(postcss([
+      require('tailwindcss'),
+      require('autoprefixer'),
+    ]))
+    .pipe(dest('./dist/css'));
+}
 
 function _build() {
+    _tailwindcss();
+    // browserify all commonsjs modules
     src('./src/cjs-modules/*.js')
         .pipe(bro())
         .pipe(dest('./dist'));
+    // move scripts
     src('./src/scripts/**/*.js')
         .pipe(dest('./dist'));
-    src(['./src/**/*.html', './src/**/*.css'])
+    // move html files
+    return src(['./src/**/*.html'])
         .pipe(dest('./dist'));
 }
 
 function _clean() {
-    del.sync('./dist');
+    return del.sync('./dist');
 }
 
 function _watch() {
-    watch(['src/*/*.js', 'src/**/*.html'], _build);
-    watch(['src/*/*.js', 'src/**/*.html']).on('change', browserSync.reload);
+    watch(['src/*/*.js', 'src/**/*.html', 'src/**/*.css'], _clean);
+    watch(['src/*/*.js', 'src/**/*.html', 'src/**/*.css'], _build);
+    watch(['src/*/*.js', 'src/**/*.html', 'src/**/*.css']).on('change', browserSync.reload);
 }
 
 function _serve() {
